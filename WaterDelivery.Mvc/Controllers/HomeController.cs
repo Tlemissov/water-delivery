d76dd1a64e2b4d584707f56acd33a248aba79f76
Zhanibek Tilemis <253250@gmail.com>
2019-02-06 14:49:03 +0600

    created admin page

--- WaterDelivery.Mvc/Controllers/HomeController.cs
+++ WaterDelivery.Mvc/Controllers/HomeController.cs
@@ -16,12 +16,6 @@ namespace WaterDelivery.Mvc.Controllers
             return View();
         }
 
-        [Authorize]
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- WaterDelivery.Mvc/Startup.cs
+++ WaterDelivery.Mvc/Startup.cs
@@ -64,6 +64,9 @@ namespace WaterDelivery.Mvc
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                  name: "areas",
+                  template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
